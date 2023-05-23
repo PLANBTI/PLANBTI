37ed4c9a6030c4aae947bc91e7b4b881a0466a61
yohan <sjohn9227@naver.com>
2023-05-23 15:28:59 +0900

    feat: order table 생성

---
+++ src/main/java/com/example/demo/aggreagate/order/entity/OrderDetail.java
@@ -0,0 +1,27 @@
+package com.example.demo.aggreagate.order.entity;
+
+import com.example.demo.aggreagate.product.entity.Product;
+import com.example.demo.base.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+@Entity
+public class OrderDetail extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Product product;
+
+    @ManyToOne
+    private Order order;
+
+    private int price;
+    private int count;
+}
