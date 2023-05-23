3e39aa26bcff3713a4ff04d1d0664c5d7d758c46
yohan <sjohn9227@naver.com>
2023-05-23 15:29:36 +0900

    feat: faq table 생성

---
+++ src/main/java/com/example/demo/aggreagate/faq/entity/Faq.java
@@ -0,0 +1,30 @@
+package com.example.demo.aggreagate.faq.entity;
+
+import com.example.demo.aggreagate.member.entity.Member;
+import com.example.demo.base.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+@Entity
+public class Faq extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Member member;
+
+    private String category;
+    private String title;
+    private String content;
+    private String email;
+
+    @Column(nullable = false)
+    private String name;
+
+}
