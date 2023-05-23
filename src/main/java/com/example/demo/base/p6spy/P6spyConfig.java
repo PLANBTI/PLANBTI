6591fc6d9e74621aced6a7dfaad888eef46aa00d
yohan <sjohn9227@naver.com>
2023-05-23 14:22:45 +0900

    feat: p6spy 도입

---
+++ src/main/java/com/example/demo/base/p6spy/P6spyConfig.java
@@ -0,0 +1,13 @@
+package com.example.demo.base.p6spy;
+
+import com.p6spy.engine.spy.P6SpyOptions;
+import jakarta.annotation.PostConstruct;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class P6spyConfig {
+    @PostConstruct
+    public void setLogMessageFormat() {
+        P6SpyOptions.getActiveInstance().setLogMessageFormat(P6spyPrettySqlFormatter.class.getName());
+    }
+}
