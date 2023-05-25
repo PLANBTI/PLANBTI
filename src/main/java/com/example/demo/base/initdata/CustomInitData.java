82ca0dc4708948e98fc8caaa3c0e9c00798295e6
yohan <sjohn9227@naver.com>
2023-05-25 15:07:33 +0900

    feat: initData추가

---
+++ src/main/java/com/example/demo/base/initdata/CustomInitData.java
@@ -0,0 +1,48 @@
+package com.example.demo.base.initdata;
+
+import com.example.demo.base.Role;
+import com.example.demo.boundedContext.member.entity.Member;
+import com.example.demo.boundedContext.member.repository.MemberRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@Profile("dev")
+@Configuration
+public class CustomInitData {
+    @Bean
+    CommandLineRunner initData(MemberRepository memberRepository, PasswordEncoder passwordEncoder) {
+
+        return new CommandLineRunner() {
+
+            @Override
+            @Transactional
+            public void run(String... args) throws Exception {
+                String encode = passwordEncoder.encode("1111");
+                Member user = Member.builder()
+                        .username("user")
+                        .password(encode)
+                        .phoneNumber("010-1111-1111")
+                        .email("user1@naver.com")
+                        .build();
+                user.addRole(Role.USER);
+
+                Member admin = Member.builder()
+                        .username("admin")
+                        .password(encode)
+                        .phoneNumber("010-1111-1111")
+                        .email("user1@naver.com")
+                        .build();
+                user.addRole(Role.USER);
+                user.addRole(Role.ADMIN);
+
+                memberRepository.save(user);
+                memberRepository.save(admin);
+            }
+        };
+    }
+}
