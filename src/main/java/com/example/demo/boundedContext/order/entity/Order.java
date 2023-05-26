74158f2f10551d3a0b41421dcf58bedd747d52ab
yohan <sjohn9227@naver.com>
2023-05-26 13:12:08 +0900

    merge: order conflict 해결

--- src/main/java/com/example/demo/boundedContext/order/entity/Order.java
+++ src/main/java/com/example/demo/boundedContext/order/entity/Order.java
@@ -3,9 +3,13 @@ package com.example.demo.boundedContext.order.entity;
 import com.example.demo.boundedContext.member.entity.Address;
 import com.example.demo.boundedContext.member.entity.Member;
 import com.example.demo.base.entity.BaseEntity;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
@@ -18,9 +22,19 @@ public class Order extends BaseEntity {
     @ManyToOne
     private Member member;
     private int price;
-    private String status;
+
     private String req;
     private int itemCount;
+
+    @Builder.Default
+    @Column(name = "order_status")
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status = OrderStatus.BEFORE;
+
+
+    @JsonManagedReference
+    @OneToMany(mappedBy = "order")
+    private List<OrderDetail> orderDetailList = new ArrayList<>();
 
     @OneToOne
     private Address address;
