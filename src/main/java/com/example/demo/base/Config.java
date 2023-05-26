98f52628aea65495dbb7c81e93030bc7a2bd2758
yohan <sjohn9227@naver.com>
2023-05-26 17:43:38 +0900

    feat: redis config 설정 및 test save및 delete 동작 확인 테스트

--- src/main/java/com/example/demo/base/Config.java
+++ src/main/java/com/example/demo/base/Config.java
@@ -2,6 +2,10 @@ package com.example.demo.base;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -9,8 +13,28 @@ public class Config {
 
     @Bean
     public RestTemplate restTemplate() {
-
         return new RestTemplate();
+    }
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory();
+    }
+
+    @Bean
+    public RedisTemplate<?,?> redisTemplate() {
+        RedisTemplate<?, ?> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+
+        // string type 저장시 아래 설정 적용됨.
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+
+        // repository 에서 저장시 hash type으로 저장되므로, 아래 설정 적용됨.
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(new StringRedisSerializer());
+
+        return  redisTemplate;
     }
 
 }
