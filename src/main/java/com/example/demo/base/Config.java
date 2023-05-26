db77c29b47f939909a791d040f2d1bf5c2dba443
yohan <sjohn9227@naver.com>
2023-05-26 13:09:31 +0900

    merge: conflict 해결

---
+++ src/main/java/com/example/demo/base/Config.java
@@ -0,0 +1,16 @@
+package com.example.demo.base;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class Config {
+
+    @Bean
+    public RestTemplate restTemplate() {
+
+        return new RestTemplate();
+    }
+
+}
