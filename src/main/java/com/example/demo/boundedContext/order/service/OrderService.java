cad3d7cd73dc113fb72913f34ea2dca92136e74e
yohan <sjohn9227@naver.com>
2023-05-30 16:35:04 +0900

    feat: order 반환시 dto로 반환, orderPage 결과 구현

--- src/main/java/com/example/demo/boundedContext/order/service/OrderService.java
+++ src/main/java/com/example/demo/boundedContext/order/service/OrderService.java
@@ -3,6 +3,7 @@ package com.example.demo.boundedContext.order.service;
 import com.example.demo.base.exception.OrderException;
 import com.example.demo.boundedContext.member.entity.Member;
 import com.example.demo.boundedContext.order.dto.OrderRequest;
+import com.example.demo.boundedContext.order.dto.OrderResponseDto;
 import com.example.demo.boundedContext.order.entity.Order;
 import com.example.demo.boundedContext.order.repository.OrderRepository;
 import com.example.demo.util.rq.ResponseData;
@@ -43,12 +44,13 @@ public class OrderService {
         order.payComplete();
     }
 
-    public ResponseData<Order> findLastOrderById(Long id) {
+    public ResponseData<OrderResponseDto> findLastOrderById(Long id) {
 
         Optional<Order> lastOrder = orderRepository.findLastOrder(id);
 
-        return lastOrder.map(order -> ResponseData.of("success", "성공", order))
-                .orElseGet(() -> ResponseData.of("fail", "실패", null));
+        return lastOrder
+                .map(order -> ResponseData.of("success", "성공", new OrderResponseDto(order)))
+                .orElseGet(() -> ResponseData.of("fail", "실패", new OrderResponseDto()));
 
     }
 
