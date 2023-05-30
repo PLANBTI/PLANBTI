4f7c39001055104da7f47296d48592cc67622620
SonHanSol1 <gksthf7869@naver.com>
2023-05-30 16:38:53 +0900

    comment: mbtiTest 주석 제거

--- src/main/java/com/example/demo/boundedContext/mbtiTest/mbtiTestController.java
+++ src/main/java/com/example/demo/boundedContext/mbtiTest/mbtiTestController.java
@@ -13,7 +13,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
