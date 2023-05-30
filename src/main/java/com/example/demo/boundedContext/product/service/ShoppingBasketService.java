972134d17e4105e6885244fc7b582d5097c288fb
yurrrrri <yu_ri_son@naver.com>
2023-05-30 13:41:30 +0900

    feat: shoppingBasket 레포지터리, 서비스 생성

---
+++ src/main/java/com/example/demo/boundedContext/product/service/ShoppingBasketService.java
@@ -0,0 +1,37 @@
+package com.example.demo.boundedContext.product.service;
+
+import com.example.demo.base.exception.DataNotFoundException;
+import com.example.demo.boundedContext.member.entity.Member;
+import com.example.demo.boundedContext.product.entity.ShoppingBasket;
+import com.example.demo.boundedContext.product.repository.ShoppingBasketRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class ShoppingBasketService {
+
+    private final ShoppingBasketRepository shoppingBasketRepository;
+
+    public ShoppingBasket findById(Long id) {
+        Optional<ShoppingBasket> shoppingBasket = shoppingBasketRepository.findById(id);
+
+        if(shoppingBasket.isEmpty()) {
+            throw new DataNotFoundException("존재하지 않는 장바구니입니다.");
+        }
+
+        return shoppingBasket.get();
+    }
+
+    public ShoppingBasket findByMember(Member member) {
+        Optional<ShoppingBasket> shoppingBasket = shoppingBasketRepository.findByMember(member);
+
+        if(shoppingBasket.isEmpty()) {
+            throw new DataNotFoundException("존재하지 않는 장바구니입니다.");
+        }
+
+        return shoppingBasket.get();
+    }
+}
