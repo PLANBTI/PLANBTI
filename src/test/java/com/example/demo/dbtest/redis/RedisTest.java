fa93fae5370501c3f170738da0ced12a2e75b324
yohan <sjohn9227@naver.com>
2023-05-30 15:17:24 +0900

    feat: redis test시 키 전부 삭제 (필요한 경우 다른 redis 커넥션 이용)

--- src/test/java/com/example/demo/dbtest/redis/RedisTest.java
+++ src/test/java/com/example/demo/dbtest/redis/RedisTest.java
@@ -1,19 +1,34 @@
 package com.example.demo.dbtest.redis;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
 
 
+@ActiveProfiles("test")
+@Transactional
 @SpringBootTest
 public class RedisTest {
 
     @Autowired
     RedisTemplate<String,String> redisTemplate;
 
+    @BeforeEach
+    void before() {
+        redisTemplate.getConnectionFactory().getConnection().flushDb();
+    }
+
+    @AfterEach
+    void after() {
+        redisTemplate.getConnectionFactory().getConnection().flushDb();
+    }
     @Test
     void t1() {
         ValueOperations<String, String> opsForValue = redisTemplate.opsForValue();
@@ -23,7 +38,5 @@ public class RedisTest {
 
         String s = opsForValue.get("user1");
         Assertions.assertThat(s).isEqualTo("hello");
-
-        redisTemplate.delete(key);
     }
 }
