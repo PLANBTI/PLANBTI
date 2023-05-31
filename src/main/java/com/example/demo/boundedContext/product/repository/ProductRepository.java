16a503c32ada30070a7da8f8e61d893fab434749
jojeongbin <cjb1009@naver.com>
2023-05-31 13:17:15 +0900

    feat : ProductRepository 생성

--- src/main/java/com/example/demo/boundedContext/product/repository/ProductRepository.java
+++ src/main/java/com/example/demo/boundedContext/product/repository/ProductRepository.java
@@ -1,7 +1,12 @@
 package com.example.demo.boundedContext.product.repository;
 
+import com.example.demo.boundedContext.member.entity.Member;
 import com.example.demo.boundedContext.product.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductRepository extends JpaRepository<Product,Long> {
+import java.util.Optional;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    Optional<Product> findById(Long id);
+
 }
