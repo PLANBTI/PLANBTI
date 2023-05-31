33b6f987bcb33ad5f32dcf7116b88c149e58817b
yohan <sjohn9227@naver.com>
2023-05-31 14:37:47 +0900

    refactor: test code mock제거

--- src/test/java/com/example/demo/boundedContext/order/service/OrderServiceTest.java
+++ src/test/java/com/example/demo/boundedContext/order/service/OrderServiceTest.java
@@ -8,16 +8,14 @@ import com.example.demo.boundedContext.order.entity.Order;
 import com.example.demo.boundedContext.order.entity.OrderDetail;
 import com.example.demo.boundedContext.order.repository.OrderRepository;
 import com.example.demo.boundedContext.product.entity.Product;
+import com.example.demo.boundedContext.product.repository.ProductRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -27,7 +25,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 @SpringBootTest
 class OrderServiceTest {
 
-    @MockBean
+    @Autowired
     OrderRepository orderRepository;
 
     @Autowired
@@ -35,26 +33,34 @@ class OrderServiceTest {
 
     @Autowired
     MemberRepository memberRepository;
+    @Autowired
+    ProductRepository productRepository;
+
+    Member member;
+    Order order;
+    @BeforeEach
+    void setUp() {
+        Product product1 = productRepository.save(Product.builder()
+                .price(1000)
+                .build());
+        Product product2 = productRepository.save(Product.builder()
+                .price(2000)
+                .build());
+        member = Member.builder().build();
+        memberRepository.save(member);
+
+        order = orderRepository.save(Order.builder()
+                .member(member)
+                .build());
+        order.addOrderDetail(OrderDetail.builder().product(product1).count(2).build());
+        order.addOrderDetail(OrderDetail.builder().product(product2).count(3).build());
+    }
 
     @DisplayName("주문 검증 성공 테스트")
     @Test
     void verifyOrderSuccess() {
-        Member member = Member.builder().id(1L).build();
-        Order order = Order.builder()
-                .member(member)
-                .build();
 
-        order.addOrderDetail(OrderDetail.builder().product(Product.builder()
-                .price(1000)
-                .build()).count(2).build());
-
-        order.addOrderDetail(OrderDetail.builder().product(Product.builder()
-                .price(2000)
-                .build()).count(3).build());
-
-        Mockito.when(orderRepository.findById(1L)).thenReturn(Optional.of(order));
-
-        OrderRequest orderRequest = new OrderRequest("1__2__3","1",8000L);
+        OrderRequest orderRequest = new OrderRequest("1__2__3",order.getUuid(),8000L);
 
         assertThat(order.getTotalAmount()).isEqualTo(8000L);
         assertThat(orderService.verifyRequest(orderRequest,member.getId())).isTrue();
@@ -63,43 +69,16 @@ class OrderServiceTest {
     @DisplayName("주문 검증 실패 테스트")
     @Test
     void verifyOrderFail() {
-        Member member = Member.builder().id(1L).build();
-        Order order = Order.builder()
-                .member(member)
-                .build();
 
-        order.addOrderDetail(OrderDetail.builder().product(Product.builder()
-                .price(1000)
-                .build()).count(2).build());
-
-        order.addOrderDetail(OrderDetail.builder().product(Product.builder()
-                .price(2000)
-                .build()).count(3).build());
-
-        Mockito.when(orderRepository.findById(1L)).thenReturn(Optional.of(order));
-
-        OrderRequest orderRequest = new OrderRequest("1__2__3","1",1000L);
+        OrderRequest orderRequest = new OrderRequest("1__2__3",order.getUuid(),1000L);
 
         assertThat(orderService.verifyRequest(orderRequest, member.getId())).isFalse();
     }
     @DisplayName("이미 계산한 경우 실패")
     @Test
     void verifyOrderStatusFail() {
-        Member member = Member.builder().id(1L).build();
-        Order order = Order.builder()
-                .member(member)
-                .build();
 
-
-        order.payComplete();
-
-        order.addOrderDetail(OrderDetail.builder().product(Product.builder()
-                .price(1000)
-                .build()).count(2).build());
-
-        Mockito.when(orderRepository.findById(1L)).thenReturn(Optional.of(order));
-
-        OrderRequest orderRequest = new OrderRequest("1__2__3","1",2000L);
+        OrderRequest orderRequest = new OrderRequest("1__2__3",order.getUuid(),2000L);
 
         assertThatThrownBy(() ->orderService.verifyRequest(orderRequest, 2L))
                 .isInstanceOf(OrderException.class);
@@ -108,14 +87,9 @@ class OrderServiceTest {
     @DisplayName("주문시 자기 소유의 order가 아닌 경우 실패")
     @Test
     void noAuthorityForOrder() {
-        Member member = Member.builder().id(1L).build();
-        Order order = Order.builder()
-                .member(member)
-                .build();
 
-        Mockito.when(orderRepository.findById(1L)).thenReturn(Optional.of(order));
 
-        OrderRequest orderRequest = new OrderRequest("1__2__3","1",1000L);
+        OrderRequest orderRequest = new OrderRequest("1__2__3","12123",1000L);
 
         assertThatThrownBy(() ->orderService.verifyRequest(orderRequest, 2L))
                 .isInstanceOf(OrderException.class);
