df07fb760f10412620188596db5b67110cb89a01
yohan <sjohn9227@naver.com>
2023-06-01 14:23:28 +0900

    feat: softDelete 추가 및 member soft delete test code 작성

--- src/main/java/com/example/demo/util/rq/Rq.java
+++ src/main/java/com/example/demo/util/rq/Rq.java
@@ -4,38 +4,36 @@ import com.example.demo.base.security.CustomOAuth2User;
 import com.example.demo.boundedContext.member.dto.MemberDto;
 import com.example.demo.boundedContext.member.entity.Member;
 import com.example.demo.boundedContext.member.repository.MemberRepository;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.annotation.RequestScope;
-
-import java.util.Optional;
 
 @Component
 @RequestScope
 public class Rq {
 
-    private final CustomOAuth2User customOAuth2User;
-    private final MemberDto member;
+    private CustomOAuth2User customOAuth2User;
+    private MemberDto member;
     private final MemberRepository memberRepository;
 
     public Rq(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
 
-        customOAuth2User = (CustomOAuth2User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        String username = customOAuth2User.getUsername();
-        Optional<Member> optionalMember = memberRepository.findByUsername(username);
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
-        if (optionalMember.isEmpty()) {
-            member = null;
-        } else {
-            Member findMember = optionalMember.get();
-            member = new MemberDto(findMember.getId(),  findMember.getUsername(), findMember.getEmail());
+        if (authentication.getPrincipal() instanceof CustomOAuth2User) {
+            this.customOAuth2User = (CustomOAuth2User) authentication.getPrincipal();
+            String username = customOAuth2User.getUsername();
+            Member findMember = memberRepository.findByUsername(username).orElseThrow();
+            member = new MemberDto(findMember.getId(), findMember.getUsername(), findMember.getEmail());
         }
     }
 
     public boolean isLogin() {
         return member != null;
     }
+
     public Long getMemberId() {
         return member.getId();
     }
