187e74af0a1db8e3c0c890dc553bf1ee9d9215a2
yohan <sjohn9227@naver.com>
2023-06-01 16:36:48 +0900

    feat: order result page

--- src/main/java/com/example/demo/boundedContext/order/controller/OrderController.java
+++ src/main/java/com/example/demo/boundedContext/order/controller/OrderController.java
@@ -2,7 +2,7 @@ package com.example.demo.boundedContext.order.controller;
 
 import com.example.demo.boundedContext.member.entity.Member;
 import com.example.demo.boundedContext.member.service.MemberService;
-import com.example.demo.boundedContext.order.dto.OrderResponseDto;
+import com.example.demo.boundedContext.order.dto.LastOrderDto;
 import com.example.demo.boundedContext.order.service.OrderService;
 import com.example.demo.util.rq.ResponseData;
 import com.example.demo.util.rq.Rq;
@@ -25,11 +25,16 @@ public class OrderController {
     public String orderPage(Model model) {
         String username = rq.getUsername();
         Member member = memberService.findByUsername(username);
-        ResponseData<OrderResponseDto> responseData = orderService.findLastOrderById(member.getId());
+        ResponseData<LastOrderDto> responseData = orderService.findLastOrderById(member.getId());
 
         model.addAttribute("order", responseData.getContent());
         return "order/orderPage";
 
+    }
+
+    @GetMapping("/result")
+    public String resultPage() {
+        return "order/result";
     }
 
 }
