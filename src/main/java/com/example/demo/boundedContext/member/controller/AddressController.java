2022316227269d351f6e0a11054f5a04ac4c725d
yurrrrri <yu_ri_son@naver.com>
2023-06-01 14:29:23 +0900

    feat: Address 필수 기능 구현, html 파일 작성

---
+++ src/main/java/com/example/demo/boundedContext/member/controller/AddressController.java
@@ -0,0 +1,83 @@
+package com.example.demo.boundedContext.member.controller;
+
+import com.example.demo.base.exception.DataNotFoundException;
+import com.example.demo.base.security.CustomOAuth2User;
+import com.example.demo.boundedContext.member.entity.Address;
+import com.example.demo.boundedContext.member.service.AddressService;
+import jakarta.validation.Valid;
+import jakarta.validation.ValidationException;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/address")
+@PreAuthorize("isAuthenticated()")
+public class AddressController {
+
+    private final AddressService addressService;
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @NotBlank
+    public static class AddressForm {
+        private String name;
+        private String addr;
+        private String addrDetail;
+        @Size(min = 5, max = 7)
+        private String zipCode;
+        private String phoneNumber;
+        private boolean isDefault;
+    }
+
+    @GetMapping("/create")
+    public String create() {
+        return "address/create";
+    }
+
+    @PostMapping("/create")
+    public String create(@Valid AddressForm form, @AuthenticationPrincipal CustomOAuth2User user) {
+        addressService.create(form.getName(), form.getAddr(), form.getAddrDetail(), form.getZipCode(), form.getPhoneNumber(), form.isDefault());
+        return "redirect:/member/profile";
+    }
+
+    @GetMapping("/modify/{id}")
+    public String modify(@PathVariable Long id, Model model) {
+        Address address = addressService.findByIdAndDeleteDateIsNull(id);
+
+        if(address == null) {
+            throw new DataNotFoundException("address not found");
+        }
+
+        model.addAttribute("address", address);
+        return "address/modify";
+    }
+
+    @PostMapping("/modify/{id}")
+    public String modify(@PathVariable Long id, @Valid AddressForm form) {
+        addressService.modify(id, form.getName(), form.getAddr(), form.getAddrDetail(), form.getZipCode(), form.getPhoneNumber(), form.isDefault());
+
+        return "redirect:/member/profile";
+    }
+
+    // soft-delete
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable Long id) {
+        Address address = addressService.findByIdAndDeleteDateIsNull(id);
+        addressService.delete(address);
+        return "redirect:/member/profile";
+    }
+}
--- src/main/java/com/example/demo/boundedContext/member/entity/Address.java
+++ src/main/java/com/example/demo/boundedContext/member/entity/Address.java
@@ -8,6 +8,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
+import java.util.Objects;
+
 @Getter
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -22,4 +24,14 @@ public class Address extends BaseEntity {
     private String phoneNumber;
     private boolean isDefault;
 
+    @Override
+    public boolean equals(Object o) {
+        Address address = (Address) o;
+        if(this.getName().equals(address.getName()) && this.getAddr().equals(address.getAddr()) && this.getAddrDetail().equals(address.getAddrDetail()) &&
+            this.getZipCode().equals(address.getZipCode()) && this.getPhoneNumber().equals(address.getPhoneNumber()) && this.isDefault == address.isDefault
+        ) {
+            return true;
+        }
+        return false;
+    }
 }
