09a18c0677a7a265274db6afa96c6a451fbeb760
jojeongbin <cjb1009@naver.com>
2023-06-05 02:15:25 +0900

    feat : 페이징 처리(3)

--- src/main/java/com/example/demo/boundedContext/product/repository/ProductRepository.java
+++ src/main/java/com/example/demo/boundedContext/product/repository/ProductRepository.java
@@ -1,12 +1,14 @@
 package com.example.demo.boundedContext.product.repository;
 
-import com.example.demo.boundedContext.member.entity.Member;
 import com.example.demo.boundedContext.product.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
     Optional<Product> findById(Long id);
+    Page<Product> findAll(Pageable pageable);
 
 }
