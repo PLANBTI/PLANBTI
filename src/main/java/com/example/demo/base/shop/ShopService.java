ffc79815f4d4e9ddfa08fc9cfe8de0b1212cde3b
jojeongbin <cjb1009@naver.com>
2023-06-05 02:12:14 +0900

    feat : 페이징 처리 기능추가

--- src/main/java/com/example/demo/base/shop/ShopService.java
+++ src/main/java/com/example/demo/base/shop/ShopService.java
@@ -3,6 +3,9 @@ package com.example.demo.base.shop;
 import com.example.demo.boundedContext.product.entity.Product;
 import com.example.demo.boundedContext.product.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,7 +15,17 @@ import java.util.List;
 public class ShopService {
     private final ProductRepository productRepository;
 
-    public List<Product> getList() {
-        return this.productRepository.findAll();
+    public Page<Product> getList(int page) {
+        Pageable pageable = PageRequest.of(page, 10);
+        return this.productRepository.findAll(pageable);
     }
+
+    /*
+    public void create(String name, String price) {
+        Product p = new Product();
+        p.getName(name);
+        p.getPrice(price);
+        this.productRepository.save(p);
+    } a
+     */
 }
