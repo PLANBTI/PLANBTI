b5ca02166331626d76449e02381ecc3a10833464
jojeongbin <cjb1009@naver.com>
2023-06-05 14:24:02 +0900

    fix : merge 충돌 해결

--- src/main/java/com/example/demo/boundedContext/product/repository/ProductRepository.java
+++ src/main/java/com/example/demo/boundedContext/product/repository/ProductRepository.java
@@ -3,6 +3,8 @@ package com.example.demo.boundedContext.product.repository;
 import com.example.demo.boundedContext.product.entity.Product;
 import jakarta.persistence.LockModeType;
 import jakarta.persistence.QueryHint;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
@@ -18,5 +20,8 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("select p from Product p where p.id = :id")
     Optional<Product> findByIdWithLock(@Param("id") Long id);
+    Optional<Product> findById(Long id);
+    Page<Product> findAll(Pageable pageable);
+
 
 }
