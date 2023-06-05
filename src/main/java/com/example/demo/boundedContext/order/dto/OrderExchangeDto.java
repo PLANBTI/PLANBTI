039bb5f1b8e7cdd030bd74132206b01d3f2584c0
yohan <sjohn9227@naver.com>
2023-06-05 21:15:02 +0900

    fix:  NotBlank error 수정

--- src/main/java/com/example/demo/boundedContext/order/dto/OrderExchangeDto.java
+++ src/main/java/com/example/demo/boundedContext/order/dto/OrderExchangeDto.java
@@ -2,6 +2,7 @@ package com.example.demo.boundedContext.order.dto;
 
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import lombok.*;
 import org.hibernate.validator.constraints.Length;
@@ -15,7 +16,7 @@ import static com.example.demo.boundedContext.order.entity.OrderItemStatus.*;
 @Getter
 public class OrderExchangeDto {
 
-    @NotBlank
+    @NotNull
     private Long orderItemId;
 
     private String productName;
