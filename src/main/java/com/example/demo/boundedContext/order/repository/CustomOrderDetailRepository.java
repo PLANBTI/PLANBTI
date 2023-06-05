23f8911413757c9f447f94891691a1357b6a2588
yohan <sjohn9227@naver.com>
2023-06-05 14:21:33 +0900

    feat: exchange를 위한 repository 및 service 구현

--- src/main/java/com/example/demo/boundedContext/order/repository/CustomOrderDetailRepository.java
+++ src/main/java/com/example/demo/boundedContext/order/repository/CustomOrderDetailRepository.java
@@ -1,9 +1,10 @@
 package com.example.demo.boundedContext.order.repository;
 
-import com.example.demo.boundedContext.order.entity.OrderDetail;
+import com.example.demo.boundedContext.order.dto.OrderExchangeDto;
 
 import java.util.Optional;
 
 public interface CustomOrderDetailRepository {
-    Optional<OrderDetail> findByIdWithMemberId(Long orderId, Long orderItemId,Long memberId);
+
+    Optional<OrderExchangeDto> findByIdWithMemberId(Long orderId, Long orderItemId, Long memberId);
 }
--- src/main/java/com/example/demo/boundedContext/order/service/OrderDetailService.java
+++ src/main/java/com/example/demo/boundedContext/order/service/OrderDetailService.java
@@ -1,6 +1,7 @@
 package com.example.demo.boundedContext.order.service;
 
 import com.example.demo.base.exception.DataNotFoundException;
+import com.example.demo.boundedContext.order.dto.OrderExchangeDto;
 import com.example.demo.boundedContext.order.entity.OrderDetail;
 import com.example.demo.boundedContext.order.repository.OrderDetailRepository;
 import lombok.RequiredArgsConstructor;
@@ -15,9 +16,9 @@ public class OrderDetailService {
 
     private final OrderDetailRepository orderDetailRepository;
 
-    public OrderDetail findOrderDetailById(Long orderId,Long orderItemId, Long memberId) {
+    public OrderExchangeDto findOrderDetailById(Long orderId, Long orderItemId, Long memberId) {
 
-        return orderDetailRepository.findByIdWithMemberId(orderId,orderItemId,memberId)
+        return orderDetailRepository.findByIdWithMemberId(orderId, orderItemId, memberId)
                 .orElseThrow(() -> new DataNotFoundException("존재하지 않는 데이터입니다"));
     }
 }
