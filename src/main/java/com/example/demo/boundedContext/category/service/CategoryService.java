2ffbcce857b169b54d5e49d807dc07a991bdc552
jojeongbin <cjb1009@naver.com>
2023-06-06 15:46:09 +0900

    fix :충돌해결

--- src/main/java/com/example/demo/boundedContext/category/service/CategoryService.java
+++ src/main/java/com/example/demo/boundedContext/category/service/CategoryService.java
@@ -3,10 +3,12 @@ package com.example.demo.boundedContext.category.service;
 import com.example.demo.boundedContext.category.entity.Category;
 import com.example.demo.boundedContext.category.repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Map;
 
+@Service
 public class CategoryService {
     /*@Autowired
     private CategoryRepository categoryRepository;
@@ -15,4 +17,5 @@ public class CategoryService {
     }
 
      */
+
 }
