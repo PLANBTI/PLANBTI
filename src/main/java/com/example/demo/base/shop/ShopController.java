f28be5473f8381c1199249c53db719c3a80ea9a5
jojeongbin <cjb1009@naver.com>
2023-06-07 22:48:36 +0900

    feat : 페이징처리 기능 수정, 검색 기능 추가

--- src/main/java/com/example/demo/base/shop/ShopController.java
+++ src/main/java/com/example/demo/base/shop/ShopController.java
@@ -1,26 +1,43 @@
 package com.example.demo.base.shop;
+import com.example.demo.boundedContext.category.entity.Category;
+import com.example.demo.boundedContext.category.repository.CategoryRepository;
+import com.example.demo.boundedContext.category.service.CategoryService;
 import com.example.demo.boundedContext.product.entity.Product;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+import static com.example.demo.boundedContext.category.entity.QCategory.category;
+import static org.springframework.data.domain.Sort.Direction.DESC;
 
 @RequiredArgsConstructor
 @Controller
 public class ShopController {
 
     private final ShopService shopService;
+    private final CategoryService categoryService;
 
     @GetMapping("/shop")
-    public String list(Model model, @RequestParam(value="page", defaultValue="0") int page) {
-        Page<Product> paging = shopService.getList(page);
+    public String list(@PageableDefault(size = 12, page = 0, direction = DESC, sort = "created") Pageable pageable,
+                       @RequestParam(value = "category", defaultValue = "istj") String category,
+                       Model model) {
+        Page<Product> paging = shopService.getList(category, pageable);
+
+        List<Category> categories = categoryService.findAll();
+
         model.addAttribute("paging", paging);
+        model.addAttribute("categories", categories);
         return "shop/shopMain";
     }
-
-
 }
 
 
