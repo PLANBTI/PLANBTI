e0518008cc71e39dcb72e6c2df6c1efb2f6b6c45
jojeongbin <cjb1009@naver.com>
2023-06-07 23:02:11 +0900

    feat : 카테고리별 화면 전환 기능 추가

--- src/main/java/com/example/demo/boundedContext/category/service/CategoryService.java
+++ src/main/java/com/example/demo/boundedContext/category/service/CategoryService.java
@@ -2,6 +2,7 @@ package com.example.demo.boundedContext.category.service;
 
 import com.example.demo.boundedContext.category.entity.Category;
 import com.example.demo.boundedContext.category.repository.CategoryRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,12 +11,12 @@ import java.util.List;
 import java.util.Map;
 
 @Service
+@RequiredArgsConstructor
 public class CategoryService {
-    /*@Autowired
-    private CategoryRepository categoryRepository;
-    public Map<Category, List<Category>> getCategory() {
-        List<Category> menulist = categoryRepository.findAll();
-    }
 
-     */
+    private final CategoryRepository categoryRepository;
+
+    public List<Category> findAll() {
+        return categoryRepository.findAll();
+    }
 }
