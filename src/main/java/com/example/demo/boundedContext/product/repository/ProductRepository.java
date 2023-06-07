c1a85b896e8e6c33dc3536bf63b75d428dee4252
jojeongbin <cjb1009@naver.com>
2023-06-07 23:23:28 +0900

    feat : 검색 기능 구현

--- src/main/java/com/example/demo/boundedContext/product/repository/ProductRepository.java
+++ src/main/java/com/example/demo/boundedContext/product/repository/ProductRepository.java
@@ -1,10 +1,13 @@
 package com.example.demo.boundedContext.product.repository;
 
 import com.example.demo.boundedContext.product.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface ProductRepository extends JpaRepository<Product, Long> {
+public interface ProductRepository extends JpaRepository<Product, Long>, ProductQueryDslRepository {
     Optional<Product> findByName(String name);
+//    Page<Product> findByTitleContaining(String searchKeyword, Pageable pageable);
 }
