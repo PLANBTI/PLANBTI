35db0b2c1567c44f4e590d53293a3ae4ec1bfddc
yurrrrri <yu_ri_son@naver.com>
2023-06-07 23:32:46 +0900

    refactor: Member Hard Delete 구현

--- src/main/java/com/example/demo/boundedContext/member/repository/MemberRepository.java
+++ src/main/java/com/example/demo/boundedContext/member/repository/MemberRepository.java
@@ -2,6 +2,8 @@ package com.example.demo.boundedContext.member.repository;
 
 import com.example.demo.boundedContext.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
@@ -11,4 +13,8 @@ public interface MemberRepository extends JpaRepository<Member, Long> ,CustomMem
     Optional<Member> findByIdAndDeleteDateIsNull(Long id);
 
     Optional<Member> findByUsernameAndDeleteDateIsNull(String username);
+
+    @Modifying
+    @Query(value = "DELETE FROM member WHERE id = ?1", nativeQuery = true)
+    void deleteHardById(Long id);
 }
