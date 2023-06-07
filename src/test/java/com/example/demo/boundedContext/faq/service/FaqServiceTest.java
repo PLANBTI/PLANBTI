fd28a261747f520e5a33c87cf2530b3780efcfc6
yurrrrri <yu_ri_son@naver.com>
2023-06-07 22:36:40 +0900

    fix: Faq, Address 테스트 오류 수정

--- src/test/java/com/example/demo/boundedContext/faq/service/FaqServiceTest.java
+++ src/test/java/com/example/demo/boundedContext/faq/service/FaqServiceTest.java
@@ -1,12 +1,12 @@
 package com.example.demo.boundedContext.faq.service;
 
 import com.example.demo.base.exception.DataNotFoundException;
+import com.example.demo.boundedContext.faq.Controller.FaqController;
 import com.example.demo.boundedContext.faq.Service.FaqService;
 import com.example.demo.boundedContext.faq.entity.Faq;
 import com.example.demo.boundedContext.faq.entity.FaqCategory;
 import com.example.demo.boundedContext.member.entity.Member;
 import com.example.demo.boundedContext.member.service.MemberService;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -43,8 +43,12 @@ public class FaqServiceTest {
     void t002() {
         Member user1 = memberService.findByUsernameAndDeleteDateIsNull("user1");
         Faq faq = faqService.findByMember(user1).get(0);
+        FaqController.FaqForm form = new FaqController.FaqForm();
+        form.setTitle("수정 제목1");
+        form.setContent("수정 내용1");
+        form.setEmail(user1.getEmail());
 
-        Faq modifiedFaq = faqService.modify(faq, "수정 제목1", "수정 내용1", user1.getEmail());
+        Faq modifiedFaq = faqService.modify(faq, form);
 
         assertThat(faq.getTitle()).isEqualTo("수정 제목1");
         assertThat(faq.getContent()).isEqualTo("수정 내용1");
