315ecc8b13e96edb1be37296f712639d076c9f54
yohan <sjohn9227@naver.com>
2023-06-08 16:02:38 +0900

    feat: 장바구니 기능 추가

--- src/main/java/com/example/demo/boundedContext/product/service/ShoppingBasketService.java
+++ src/main/java/com/example/demo/boundedContext/product/service/ShoppingBasketService.java
@@ -2,18 +2,25 @@ package com.example.demo.boundedContext.product.service;
 
 import com.example.demo.base.exception.handler.DataNotFoundException;
 import com.example.demo.boundedContext.member.entity.Member;
+import com.example.demo.boundedContext.member.service.MemberService;
+import com.example.demo.boundedContext.product.entity.Product;
 import com.example.demo.boundedContext.product.entity.ShoppingBasket;
 import com.example.demo.boundedContext.product.repository.ShoppingBasketRepository;
+import com.example.demo.util.rq.ResponseData;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
+
+import static com.example.demo.util.rq.ResponseData.Status.SUCCESS;
 
 @Service
 @RequiredArgsConstructor
 public class ShoppingBasketService {
 
     private final ShoppingBasketRepository shoppingBasketRepository;
+    private final MemberService memberService;
+    private final ProductService productService;
 
     public ShoppingBasket findById(Long id) {
         Optional<ShoppingBasket> shoppingBasket = shoppingBasketRepository.findById(id);
@@ -33,5 +40,25 @@ public class ShoppingBasketService {
         }
 
         return shoppingBasket.get();
+    }
+
+
+    //TODO 파사드
+    public ResponseData<String> addProduct(Long memberId, Long productId) {
+
+        Member member = memberService.findById(memberId);
+        Product product = productService.findById(productId);
+        ShoppingBasket shoppingBasket = ShoppingBasket
+                .builder()
+                .build();
+        shoppingBasket.addProduct(member,product);
+
+        shoppingBasketRepository.save(shoppingBasket);
+
+
+        return ResponseData.<String>builder()
+                .statusCode(SUCCESS)
+                .msg("장바구니에 상품을 담았습니다.")
+                .build();
     }
 }
