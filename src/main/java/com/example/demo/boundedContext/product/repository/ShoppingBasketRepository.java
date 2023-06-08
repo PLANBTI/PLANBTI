6644bfcbdadc788376fac73c7d64e79321bf140b
yohan <sjohn9227@naver.com>
2023-06-08 16:32:43 +0900

    feat: 장바구니 테스트 코드 추가 및 장바구니 서비스 기능 리팩토링

--- src/main/java/com/example/demo/boundedContext/product/repository/ShoppingBasketRepository.java
+++ src/main/java/com/example/demo/boundedContext/product/repository/ShoppingBasketRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface ShoppingBasketRepository extends JpaRepository<ShoppingBasket, Long> {
-    Optional<ShoppingBasket> findByMember(Member member);
+    Optional<ShoppingBasket> findByMemberId(Long id);
 }
