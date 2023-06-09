82958745a1b2b4d6bde34d141df262871057b006
jojeongbin <cjb1009@naver.com>
2023-06-09 21:18:41 +0900

    feat : @ConfigurationPropertiesScan 추가

--- src/main/java/com/example/demo/ProjectShopApplication.java
+++ src/main/java/com/example/demo/ProjectShopApplication.java
@@ -2,6 +2,8 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -11,6 +13,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableCaching
 @EnableJpaAuditing
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class ProjectShopApplication {
 
     public static void main(String[] args) {
