1cc845162f43eb3030ef99f8d3f0fe1d087b2949
yohan <sjohn9227@naver.com>
2023-06-10 21:55:51 +0900

    refactor: 주석제거

--- src/main/java/com/example/demo/boundedContext/product/repository/ProductRepository.java
+++ src/main/java/com/example/demo/boundedContext/product/repository/ProductRepository.java
@@ -1,8 +1,6 @@
 package com.example.demo.boundedContext.product.repository;
 
 import com.example.demo.boundedContext.product.entity.Product;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
