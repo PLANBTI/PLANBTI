ce70064549eea8df01a7cb78ea8b64e170dd8a46
yurrrrri <yu_ri_son@naver.com>
2023-06-13 01:30:01 +0900

    feat: 관리자 매출 현황 페이지 구현

--- src/main/java/com/example/demo/boundedContext/order/repository/orderdetail/OrderDetailRepository.java
+++ src/main/java/com/example/demo/boundedContext/order/repository/orderdetail/OrderDetailRepository.java
@@ -3,9 +3,10 @@ package com.example.demo.boundedContext.order.repository.orderdetail;
 import com.example.demo.boundedContext.order.entity.OrderDetail;
 import com.example.demo.boundedContext.order.repository.orderdetail.CustomOrderDetailRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 public interface OrderDetailRepository extends JpaRepository<OrderDetail,Long>, CustomOrderDetailRepository {
-
+    List<OrderDetail> findByCreateDateBetween(LocalDateTime startDate, LocalDateTime endDate);
 }
