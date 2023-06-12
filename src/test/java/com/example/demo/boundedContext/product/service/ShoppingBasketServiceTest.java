2ec1477469ba356d765ead48d1ea29b64ff41adb
yohan <sjohn9227@naver.com>
2023-06-12 15:13:37 +0900

    feat: 장바구니 삭제 기능 추가

--- src/test/java/com/example/demo/boundedContext/product/service/ShoppingBasketServiceTest.java
+++ src/test/java/com/example/demo/boundedContext/product/service/ShoppingBasketServiceTest.java
@@ -2,14 +2,17 @@ package com.example.demo.boundedContext.product.service;
 
 import com.example.demo.boundedContext.member.entity.Member;
 import com.example.demo.boundedContext.member.repository.MemberRepository;
+import com.example.demo.boundedContext.product.entity.Basket;
 import com.example.demo.boundedContext.product.entity.Product;
 import com.example.demo.boundedContext.product.entity.ShoppingBasket;
+import com.example.demo.boundedContext.product.repository.BasketRepository;
 import com.example.demo.boundedContext.product.repository.ProductRepository;
 import com.example.demo.util.rq.ResponseData;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -29,6 +32,9 @@ class ShoppingBasketServiceTest {
     @Autowired
     ShoppingBasketService shoppingBasketService;
 
+    @Autowired
+    BasketRepository basketRepository;
+
     @DisplayName("장바구니 추가 성공")
     @Test
     void t1() {
@@ -45,6 +51,30 @@ class ShoppingBasketServiceTest {
         ShoppingBasket basket = shoppingBasketService.findByMember(member.getId());
 
         assertThat(basket.getMember().getId()).isEqualTo(member.getId());
+    }
+
+    @DisplayName("장바구니 삭제 기능 성공")
+    @Test
+    void t2() {
+        Member member = Member.builder().build();
+        memberRepository.save(member);
+
+        Product product = Product.builder().build();
+        productRepository.save(product);
+
+        Product product2 = Product.builder().build();
+        productRepository.save(product2);
+
+        shoppingBasketService.addProduct(member.getId(), product.getId(),1);
+        shoppingBasketService.addProduct(member.getId(), product2.getId(),2);
+
+
+        ResponseEntity<Void> delete = shoppingBasketService.delete(product2.getId(), member.getId());
+
+        Basket basket = basketRepository.findById(member.getId()).orElseThrow();
+        assertThat(delete.getStatusCode().is2xxSuccessful()).isTrue();
+        assertThat(basket.getCount()).isEqualTo(1);
+        assertThat(basket.getProducts().get(0).getId()).isEqualTo(product.getId());
     }
 
 }
