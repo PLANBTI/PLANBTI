4b6ab1c63abe1b7f48e6d981ccad6827d1a158d4
yohan <sjohn9227@naver.com>
2023-06-14 14:58:15 +0900

    feat: 모니터링 툴 log 확인

--- src/main/java/com/example/demo/base/security/filter/PrometheusAuthenticationFilter.java
+++ src/main/java/com/example/demo/base/security/filter/PrometheusAuthenticationFilter.java
@@ -4,6 +4,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -16,6 +17,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 
+@Slf4j
 @Component
 public class PrometheusAuthenticationFilter extends OncePerRequestFilter {
 
@@ -25,14 +27,43 @@ public class PrometheusAuthenticationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
+        String ip = request.getHeader("X-Forwarded-For");
+
+        if(ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if(ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if(ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_CLIENT_IP");
+        }
+        if(ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+        }
+        if(ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
+
         boolean requestIp = request.getRequestURI().equals("/actuator/prometheus");
-        boolean equalIp = request.getRemoteHost().equals(prometheusIp);
+        boolean equalIp = ip.equals(prometheusIp);
+        String requestURL = request.getRequestURL().toString();
+        String remoteIP = request.getRemoteAddr();
+
+        log.info("진짜 ip: {}",ip);
+        log.info("요청 주소: {}", request.getRequestURI());
+        log.info("요청한 사람의 ip: {}",request.getRemoteHost());
+        log.info("요청한 사람의 port: {}",request.getRemotePort());
+        log.info("주소 검증: {}", requestIp);
+        log.info("ip 검증: {}",equalIp);
+        log.info("요청한 사람의 통합 주소: {}",requestURL);
+        log.info("remoteIP: {}",remoteIP);
 
         if (requestIp && equalIp) {
 
             Authentication authentication = new UsernamePasswordAuthenticationToken("prometheus", null, new ArrayList<>(Collections.singleton(new SimpleGrantedAuthority("ROLE_ADMIN"))));
             SecurityContextHolder.getContext().setAuthentication(authentication);
-
+            log.info("프로메테우스 통과");
             filterChain.doFilter(request, response);
 
         } else {
