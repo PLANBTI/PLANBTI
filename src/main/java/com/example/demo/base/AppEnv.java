c0ed657ce635f23875688d911b8cb77be1c2c560
SonHanSol1 <gksthf7869@naver.com>
2023-06-14 15:15:49 +0900

    refactor: 코드리뷰 리펙토링 및 prod.yml 수정

--- src/main/java/com/example/demo/base/AppEnv.java
+++ src/main/java/com/example/demo/base/AppEnv.java
@@ -4,4 +4,6 @@ public class AppEnv {
 
     public static final String tossPayRequestUrl = "https://api.tosspayments.com/v1/payments/confirm";
     public static final String tossRefundRequestUrl = "https://api.tosspayments.com/v1/payments";
+
+    public static final String imageUrl = "https://planbti.cdn.ntruss.com/";
 }
--- src/main/java/com/example/demo/boundedContext/member/entity/MbtiTest.java
+++ src/main/java/com/example/demo/boundedContext/member/entity/MbtiTest.java
@@ -2,6 +2,7 @@ package com.example.demo.boundedContext.member.entity;
 
 import com.example.demo.base.entity.BaseEntity;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.ManyToOne;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
@@ -17,9 +18,8 @@ public class MbtiTest extends BaseEntity {
     private String result;
     private String title;
     private String content;
-
     private String testImgUrl;
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     private Member member;
 
 }
